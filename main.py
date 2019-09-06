24746c2770b2ad8f807b9166d0112926160b8074
Jasmon Cooley <jasmon.cooley@bison.howard.edu>
2019-09-06 02:30:28 +0000

    code

---
+++ main.py
@@ -0,0 +1,28 @@
+def createNewFile(file,rightMarg,leftMarg):
+  
+  max =50
+  length = 0
+  margRSpace = " " * rightMarg
+  margLSpace = " " * leftMarg
+  t= open(file,"r+")
+  f= open("DAT1.txt","w+")
+  
+  old = t.readlines()
+  print(old)
+  f.write(str(margLSpace))
+  for line in old:
+    for word in line.split():
+      length += len(word) +1
+      if len(margRSpace)+ length+ len(margLSpace) > max:
+        length =0
+        f.write(str(margRSpace))
+        f.write(str('\n'))
+        f.write(str(margLSpace))
+        f.write(str(word)+ " ")
+      else:
+        f.write(str(word) + " ")
+  
+  
+if __name__ == "__main__":
+    createNewFile("data.txt",6,5)
+  
